fe52342fd7e883dee46215773adcb78b46dc7a4b
Can Özcan <canozcanstu@gmail.com>
2022-11-25 16:32:28 +0300

    REST Api With MongoDb - Redux - Admin Dashboard

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,14 @@
+import axios from "axios";
 import Head from "next/head";
+import { useState } from "react";
 import Featured from "../components/Featured";
 import PizzaList from "../components/PizzaList";
+import Add from "../components/Add";
 import styles from "../styles/Home.module.css";
+import AddButton from "../components/AddButton";
 
-export default function Home() {
+export default function Home({ pizzaList, admin }) {
+  const [close, setClose] = useState(true);
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +17,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
-      <PizzaList />
+      {admin && <AddButton setClose={setClose} />}
+      <PizzaList pizzaList={pizzaList} />
+      {!close && <Add setClose={setClose} />}
     </div>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
+
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
+  }
+
+  const res = await axios.get("http://localhost:3000/api/products");
+  return {
+    props: {
+      pizzaList: res.data,
+      admin,
+    },
+  };
+};
